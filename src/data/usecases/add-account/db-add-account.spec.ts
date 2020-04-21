42e0c3c942b1d1337b9f2791669ab760b1859a35
daniel sena <danielsena04@gmail.com>
2020-04-21 18:04:57 -0300

    test: ensure DbAddAccount throws if encrypter throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -33,4 +33,16 @@ describe('DbAddAccount Usecase', () => {
     await dbAddAccount.add(accountData)
     expect(encryptSpy).toHaveBeenCalledWith(accountData.password)
   })
+
+  test('Should throw if encrypter throws', async () => {
+    const { dbAddAccount, encrypterStub } = dbAddAccountFactory()
+    jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const accountPromise = dbAddAccount.add(accountData)
+    await expect(accountPromise).rejects.toThrow()
+  })
 })

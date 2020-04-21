4ad139e3af352960a9c1c9374f82ccf2ad6bb94a
daniel sena <danielsena04@gmail.com>
2020-04-20 22:20:36 -0300

    test: ensure SignupController returns 200 if valid data is provided

--- src/presentation/controller/signup/signup.spec.ts
+++ src/presentation/controller/signup/signup.spec.ts
@@ -193,4 +193,24 @@ describe('Signup Controller', () => {
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   })
+
+  test('Should return 200 if an valid data is provided', () => {
+    const { signupController } = signupFactory()
+    const httpRequest = {
+      body: {
+        email: 'valid_email@mail.com',
+        name: 'valid_name',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password'
+      }
+    }
+    const httpResponse = signupController.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    })
+  })
 })

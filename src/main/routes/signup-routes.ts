8e46e71d31503c3b35de6396d78c82e549505c1e
daniel sena <danielsena04@gmail.com>
2020-06-02 19:11:06 -0300

    refactor: improve file names and move email-validator to main layer

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { signupControllerFactory } from '../factories/signup/signup-factory'
-import { routesAdapter } from '../adapters/express-adapter'
+import { routesAdapter } from '../adapters/express/express-adapter'
 
 export default (router: Router): void => {
   router.post('/signup', routesAdapter(signupControllerFactory()))

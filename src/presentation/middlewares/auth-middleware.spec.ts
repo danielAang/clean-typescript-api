b1c8ecd92f992802826dfb5bfac3898e8233b706
daniel sena <danielsena04@gmail.com>
2020-07-06 20:17:47 -0300

    test: ensure AuthMiddleware returns 200 if LoadAccountByToken succeeds

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,5 +1,5 @@
 import { AccessForbiddenError } from '../errors'
-import { forbidden } from '../helpers/http/http-helper'
+import { ok, forbidden } from '../helpers/http/http-helper'
 import { AuthMiddleware } from './auth-middleware'
 import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
 import { AccountModel } from '../../domain/models/account'
@@ -62,5 +62,11 @@ describe('Auth Middleware', () => {
     jest.spyOn(loadAccountByTokenStub, 'load').mockReturnValueOnce(new Promise((resolve) => resolve(null)))
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(forbidden(new AccessForbiddenError()))
+  })
+
+  test('Should return 200 LoadAccountByToken returns an account', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(ok({ accountId: 'any_id' }))
   })
 })
